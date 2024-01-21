7988811eba4c4d66322ae2c2645c321c289e2bab
AGL97 <alejandro41.gl@gmail.com>
2024-01-20 23:53:55 -0500

    Optimizado cambio de color
    Implementado metodo de cambio por nombre de clases
    Implementado cambio por escucha de eventos
    Agregados 2 clases para cambio de color
    Agregado colores a cada clase en el style

--- cambiaColor.js
+++ cambiaColor.js
@@ -1,13 +1,28 @@
-document.getElementById("photo").onclick = function()
-{
-    var refColor = document.querySelector("h1").style.color
-    
-    if(refColor == "black")
+let PHOTO = document.getElementById("photo");
+
+/*
+PHOTO.addEventListener("click",function()
+{    
+    if(document.querySelector("header h1").style.color == "black")
     {
-        document.querySelector("h1").style.color = "blue"
+        document.querySelector("header h1").style.color = "blue";
     }
     else
     {
-        document.querySelector("h1").style.color = "black"
+        document.querySelector("header h1").style.color = "black";
     }
-}
+}) 
+*/
+
+PHOTO.addEventListener("click",function()
+{    
+    if(document.querySelector("header h1").className == "claseColor1")
+    {
+        document.querySelector("header h1").className = "claseColor2";
+    }
+    else
+    {
+        document.querySelector("header h1").className = "claseColor1";
+    }
+    
+})
